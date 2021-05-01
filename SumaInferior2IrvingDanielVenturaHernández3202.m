6b1da0e39c48eaa6d862e24b1d76047e9d9eb398
Irving Ventura <39660540+CarlosAlbertoV@users.noreply.github.com>
2021-05-01 02:27:19 -0500

    Add files via upload

---
+++ SumaInferior2IrvingDanielVenturaHernández3202.m
@@ -0,0 +1,23 @@
+% Titulo: Suma Interiores (2)
+% Nombre: Irving Daniel Ventura Hernández
+% Grupo: 3202
+% Fecha: Viernes 30 de Abril del 2021
+
+% f (x) = x ^ (2) +2
+
+% De variables limpiar
+claro
+funcion [int] = área (desvío, a, b, n)
+f = en linea (divertido);
+% paso de la funcion
+% Base de los rectangulos
+h = (ba) / n;
+% Numero de intervalos
+x = a: h: b;
+int = 0 ;
+% Inicia un ciclo
+para i = 1: n
+% Calcular punto medio
+xn = (x (i) + x (i + 1 )) / 2 ;
+% Valor del area de uno de los rectangulos
+int = int + f (xn) * h;
